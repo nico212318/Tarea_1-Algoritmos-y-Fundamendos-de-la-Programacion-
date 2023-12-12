e1e63f046193c6f1a289490d339f21de076c3b64
nico212318 <151861323+nico212318@users.noreply.github.com>
2023-12-12 11:22:39 -0500

    Add files via upload

---
+++ Tarea 1.py
@@ -0,0 +1,50 @@
+##########################################################
+#################### Primer programa #####################
+##########################################################
+
+# El proposito de este programa es el de aprender las cosas basicas que sirven
+# hasta en las programaciones mas avazanda.
+
+print("Programa 1")
+a = 5
+b = 7
+d = 6.8
+e = 5.5
+
+c = a +b  #Suma de varibles enteras
+f = d - e   #Resta de variables Float
+g = a * b   #Multiplicacion de variables enteras
+h = d / e   #Divicion de variables float
+
+print("suma =" , c)
+print("resta =" , f)
+print("multiplicacion =" , g)
+print("division  =" , h)
+print("Este proograma funciona gracicas a las matematicas y lecutura de datos de pycharm ya que no es necesario "
+      "declarar las variables como float o int.")
+##########################################################
+#################### Segundo Programa ####################
+##########################################################
+
+print("Programa 2")
+Nombres = "Edgar Nicolas"
+Apellidos = "Flores Once"
+longitud = len(Nombres + Apellidos)
+print("Nombre : " + Nombres , Apellidos , longitud) # Se suma el print con las variables para que salga la longitud de los datos
+print("El proposito de este programa es el de contar el numero de caracteres puestos en las variables"
+      " Nombre y Apellidos.")
+
+##########################################################
+#################### Tercer Programa #####################
+##########################################################
+print("Programa 3")
+
+numero1 = 233
+numero2 = 234.56
+cambio1 = float(numero1)
+cambio2 = int(numero2)
+print("Numero entero =",numero1, "Numero con decimal =", cambio1 )
+print("Numero con decimal =",numero2, "Numero entero =", cambio2)
+
+print("Este programa va a servir cuando necesitemos numeros enteros en la programacion "
+      "ya que en la mayoria de proyectos de programacion se necesitan estos numeros.")
